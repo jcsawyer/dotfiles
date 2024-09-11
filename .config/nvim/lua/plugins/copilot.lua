7ee74e4bd3a69eb028d5df304ae56a3a58c4fb9d
Joseph Sawyer <joseph@jcsawyer.com>
2024-09-11 12:31:41 +0100

    cleanup nvim config

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -20,18 +20,6 @@ local prompts = {
 return {
   "zbirenbaum/copilot-cmp",
   { "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter" },
-  --{
-  --  "CopilotC-Nvim/CopilotChat.nvim",
-  --  branch = "canary",
-  --  dependencies = {
-  --    { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-  --    { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-  --  },
-  --  build = "make tiktoken", -- Only on MacOS or Linux
-  --  opts = {
-  --    debug = false, -- Enable debugging
-  --  },
-  --},
   {
     { import = "plugins.extras.copilot-vim" }, -- Or use { import = "lazyvim.plugins.extras.coding.copilot" },
     {
