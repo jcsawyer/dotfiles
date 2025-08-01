5281b0377d2b8c1747dd8f0230121d34e30166f1
Joseph Sawyer <joseph@jcsawyer.com>
2025-08-01 22:38:48 +0100

    updates and add avalonia lsp

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Avalonia LSP
+local avalonia_lsp_bin = "~/.vscode/extensions/avaloniateam.vscode-avalonia-0.0.32/avaloniaServer/AvaloniaLanguageServer.dll"
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.axaml" },
+  callback = function()
+    vim.cmd.setfiletype("xml")
+    vim.lsp.start({
+      name = "Avalonia LSP",
+      cmd = {
+        "dotnet",
+        avalonia_lsp_bin,
+      },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
+})
--- .zshrc
+++ .zshrc
@@ -1,10 +1,8 @@
-export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
-source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
-source /opt/homebrew/opt/chruby/share/chruby/auto.sh
-chruby ruby-3.1.3 # run chruby to see actual version
-export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+#export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+export PATH="/Users/jcsawyer/.rbenv/versions/3.1.0/bin:$PATH"
 alias config="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 
 # Load Angular CLI autocompletion.
 source <(ng completion script)
+export PATH="/usr/local/homebrew/opt/binutils/bin:$PATH"
